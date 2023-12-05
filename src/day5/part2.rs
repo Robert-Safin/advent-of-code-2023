7d69a2a7121c98af70d052c1c9bf64874966b588
Robert Safin <robertsafin98@gmail.com>
2023-12-05 19:34:58 +0800

    5

---
+++ src/day5/part2.rs
@@ -0,0 +1,124 @@
+pub fn solution(input: String) -> i64 {
+  let blocks: Vec<_> = input.split("\n\n").collect();
+  if blocks.len() < 8 {
+      return -1;
+  }
+
+  let seed_ranges = match parse_seed_ranges(&blocks[0]) {
+      Some(ranges) => ranges,
+      None => {
+          return -1;
+      }
+  };
+
+  let mut all_seeds = Vec::new();
+  for (start, length) in seed_ranges {
+      all_seeds.extend(start..start + length);
+  }
+
+  let mut maps: Vec<Vec<((i64, i64), i64)>> = Vec::new();
+  for block in &blocks[1..=7] {
+      let map = parse_map_to_ranges(block);
+      maps.push(map);
+  }
+
+  let smallest = all_seeds
+      .iter()
+      .map(|&seed| transform_through_stages_with_ranges(seed, &maps))
+      .min()
+      .unwrap_or(-1);
+    println!("Smallest: {}", smallest);
+    smallest
+}
+
+fn parse_seed_ranges(input: &str) -> Option<Vec<(i64, i64)>> {
+  input
+      .split(": ")
+      .nth(1)?
+      .split_whitespace()
+      .map(|s| s.parse::<i64>().ok())
+      .collect::<Option<Vec<i64>>>()?
+      .chunks_exact(2)
+      .map(|chunk| Some((chunk[0], chunk[1])))
+      .collect()
+}
+
+
+
+fn parse_map_to_ranges(input: &str) -> Vec<((i64, i64), i64)> {
+  input
+      .split(":\n")
+      .skip(1)
+      .next()
+      .unwrap()
+      .split('\n')
+      .filter_map(|line| {
+          let parts: Vec<i64> = line
+              .split_whitespace()
+              .filter_map(|num| num.parse().ok())
+              .collect();
+          if parts.len() == 3 {
+              let (start_dest, start_source, length) = (parts[0], parts[1], parts[2]);
+              Some(((start_source, start_source + length), start_dest))
+          } else {
+              None
+          }
+      })
+      .collect()
+}
+
+fn transform_through_stages_with_ranges(seed: i64, maps: &[Vec<((i64, i64), i64)>]) -> i64 {
+  maps.iter()
+      .fold(seed, |current, map| transform_with_range_map(current, map))
+}
+
+fn transform_with_range_map(seed: i64, map: &[((i64, i64), i64)]) -> i64 {
+  map.iter()
+      .find(|&&((start, end), _)| seed >= start && seed < end)
+      .map_or(seed, |&((start, _), dest_start)| {
+          dest_start + (seed - start)
+      })
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  #[test]
+  fn test_brief() {
+      let input = "seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4"
+          .to_string();
+      assert_eq!(solution(input), 46);
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use advent_of_code_2023::day4;
+use advent_of_code_2023::day5;
 
 
 
 
 fn main() {
-  let input: String = std::fs::read_to_string("src/inputs/day4.txt").unwrap();
-  day4::part2::solution(input);
+  let input: String = std::fs::read_to_string("src/inputs/day5.txt").unwrap();
+  day5::part1::solution(input);
 }
