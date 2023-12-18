249bf267955e405f781396f93b3267b8e3e71060
Robert Safin <robertsafin98@gmail.com>
2023-12-18 17:39:59 +0800

    18

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use advent_of_code_2023::day17;
+use advent_of_code_2023::day18;
 
 fn main() {
-    let input: String = std::fs::read_to_string("src/inputs/day17.txt").unwrap();
-    day17::part2::solution(&input);
+    let input: String = std::fs::read_to_string("src/inputs/day18.txt").unwrap();
+    day18::part2::solution(input);
 }
